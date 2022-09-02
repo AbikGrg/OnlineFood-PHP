17e2553c792022cf0e6b575cafce0d24d68210cc
Abik Gurung <abik.gurung.2000@gmail.com>
2022-09-02 19:57:27 +0545

    first commit

---
+++ admin/image.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <form action='image.php' method='post' enctype='multipart/form-data'>
+            <lable>Name</lable>
+            <input type="text" name="name">
+            <br>
+            <label>Select image to upload</label>
+            <input type="file" name="image">
+            <input type='submit' name='submit' value='Upload the picture'>
+        </form>
+    </body> 
+</html>
